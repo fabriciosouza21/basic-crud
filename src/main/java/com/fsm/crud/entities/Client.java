9d164a73bf69b97026ff8daded5b0c537f4fe15a
fabricio21777 <fabriciosouza21777@gmail.com>
2021-09-16 15:56:25 -0300

    maping entity Client in jpa and seed database

--- src/main/java/com/fsm/crud/entities/Client.java
+++ src/main/java/com/fsm/crud/entities/Client.java
@@ -1,5 +1,109 @@
 package com.fsm.crud.entities;
 
-public class Client {
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Integer children;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant bithDate;
+	
+	
+	public Client() { }
+
+	public Client(Long id, String name, String cpf, Double income, Instant bithDate, Integer children) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.bithDate = bithDate;
+		this.children = children;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBithDate() {
+		return bithDate;
+	}
+
+	public void setBithDate(Instant bithDate) {
+		this.bithDate = bithDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
 }
